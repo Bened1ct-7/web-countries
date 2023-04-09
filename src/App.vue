2fba308fad8780e0f1e6baf31a904cf5ed109fe6
Benedict Umeozor <111297090+Bened1ct-7@users.noreply.github.com>
2023-04-09 09:36:53 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { onMounted } from "vue"
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
+
+const app = JSON.parse(localStorage.getItem("theme")) || "";
+
+onMounted(() => {
+  document.body.className = app.theme;
+})
+
+</script>
+
+<template>
+  <Header />
+  <RouterView />
+  <Footer />
+</template>
